dfdc964fce741f93aa77f4cfcfe9f949f809ec67
Quinten <q.m.b@hotmail.com>
2021-06-02 15:26:40 +0200

    More refactoring

--- src/view/weatherView.js
+++ src/view/weatherView.js
@@ -2,25 +2,45 @@ export default class weatherView {
 
     controller;
 
-    text;
-    button;
-    lat;
-    lon;
-
     constructor(controller) {
         this.controller = controller;
-        this.text = document.getElementById('weatherText');
-        this.button = document.getElementById('weatherButton');
-        this.lat = document.querySelector('#latWeather');
-        this.lon = document.querySelector('#lonWeather');
 
-        this.button.addEventListener("click", () => {
+        const info = document.getElementById("weatherInfo");
+        const api = document.getElementById("api");
+
+        const btn = document.createElement('btn');
+        const text = document.createElement('h2');
+        const lat = document.createElement("input");
+        const long = document.createElement("input");
+
+        lat.id = "latWeather";
+        long.id = "longWeather";
+        lat.type = "text";
+        long.type = "text";
+        text.innerText = "No info yet";
+        text.id = "infoText";
+        text.className = "ml-4";
+        btn.innerText = "Wijzig locatie";
+        btn.className = "ml-4 btn btn-success";
+
+        api.appendChild(lat);
+        api.appendChild(long);
+        api.appendChild(btn);
+
+        info.appendChild(text);
+
+        btn.addEventListener("click", () => {
             // console.log('Values: lat: ' + this.lat.value + ', lon: ' + this.lon.value);
-            controller.update(this.lat.value, this.lon.value);
+            // controller.update(this.lat.value, this.lon.value);
+
+            const long = 5.85462200
+            const lat = 51.84286700;
+            controller.update(lat, long);
         });
     }
 
     update(string){
-        this.text.innerText = 'Weer: ' + string;
+        const text = document.getElementById("infoText");
+        text.innerText = 'Weer: ' + string;
     }
 }
